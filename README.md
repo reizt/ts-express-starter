edb7471d405a0a0ddf55201b892f4d9c6e1157f8
reizt <reizt.dev@gmail.com>
2022-12-07 18:06:59 +0900

    Make it possible to specify the port with the -p option

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ yarn install
 yarn dev
 ```
 
+or
+
+```
+yarn dev -p 4157
+```
+
+to specify server port
+
 nodemon watches changes of files in `src` and process respawns automatically when you save code.
 
 ## Test
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res, next) => {
   const message = 'Hello world!';
   res.status(200).send({ message });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+const DEFAULT_PORT = 3000;
+let port = DEFAULT_PORT;
+for (let i = 0; i < process.argv.length; i++) {
+  if (process.argv[i] === '-p') {
+    const nextArg = process.argv[i + 1];
+    if (nextArg != null) {
+      const portInt = parseInt(nextArg);
+      if (!isNaN(portInt)) port = portInt;
+    }
+    i++;
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
